56455486e4c55d28b42f08d5f368463693426b59
Mateo <monorymateo1@gmail.com>
2025-03-06 13:41:04 -0500

    listo

---
+++ script.js
@@ -0,0 +1,18 @@
+function descargarContacto() {
+    const vcardData = `BEGIN:VCARD
+VERSION:3.0
+N:Gómez;Juan;Alejandro;;
+FN:Juan Alejandro Gómez
+TEL:+57 3118814207
+EMAIL;TYPE=WORK: ventasarmenia@cervalle.com
+END:VCARD`;
+
+    const blob = new Blob([vcardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contacto.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
